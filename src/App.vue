69ae5dfedd29460d9085e5be7b15a183854e3058
王勇 <wy19945372694@163.com>
2023-02-15 09:46:52 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,30 @@
 <template>
     <div id="app">
-        <!-- <TreeController msg="..." class="tree-controller" /> -->
-        <!-- <CarouselTool /> -->
-        <!-- <TabsTool class="tabs" /> -->
-        <!-- <DynamicRefTest /> -->
-        <!-- <CollapseTool /> -->
-        <!-- <SelectedHighlight/> -->
-        <TestTool />
+        <el-button type="primary" @click="add()">加1</el-button>
+        <el-collapse>
+            <el-collapse-item v-for="item in list" :key="item.id">
+                <el-tabs>
+                    <el-tab-pane label="A">A</el-tab-pane>
+                    <el-tab-pane label="B">B</el-tab-pane>
+                </el-tabs>
+            </el-collapse-item>
+        </el-collapse>
     </div>
 </template>
 
 <script>
-import TestTool from "@/components/test.vue"
+import { nanoid } from "nanoid"
 export default {
     name: "App",
-    components: {
-        TestTool
+    data() {
+        return {
+            list: []
+        }
+    },
+    methods: {
+        add() {
+            this.list.push({ id: nanoid() })
+        }
     }
 }
 </script>
@@ -29,11 +38,17 @@ export default {
     color: #2c3e50;
     margin-top: 60px;
 }
+
 .tree-controller {
     margin-bottom: 50px;
 }
 
 .tabs {
     transform: translateX(50%);
+}
+
+.wrraper {
+    display: flex;
+    justify-content: space-between;
 }
 </style>
--- src/utils/Element.js
+++ src/utils/Element.js
@@ -1,7 +1,8 @@
 import Vue from "vue"
 import "element-ui/lib/theme-chalk/index.css"
-import { Tree, Tag, Carousel, CarouselItem, Tabs, TabPane, Collapse, CollapseItem } from "element-ui"
+import { Button, Tree, Tag, Carousel, CarouselItem, Tabs, TabPane, Collapse, CollapseItem, RadioGroup, Radio } from "element-ui"
 
+Vue.use(Button)
 Vue.use(Tree)
 Vue.use(Tag)
 Vue.use(Carousel)
@@ -10,3 +11,5 @@ Vue.use(Tabs)
 Vue.use(TabPane)
 Vue.use(Collapse)
 Vue.use(CollapseItem)
+Vue.use(RadioGroup)
+Vue.use(Radio)
